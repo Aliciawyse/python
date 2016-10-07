3f0f443440f5259a705a21788ed35fda84d75f02
Alicia <aliciawyse@gmail.com>
2016-10-07 17:48:31 +0000

    added more practice problems

--- ch12/app_quiz.py
+++ ch12/app_quiz.py
@@ -3,19 +3,31 @@ import csv
 fhand = open('TSE_sample_data.csv')
 csv_f = csv.reader(fhand)
 
-date2words = dict()
+#Create a disctionary to store a key-value pairs.
+#In this scenario, a key is a users start date and a value is the corresponding word in the words column.
+date_and_word = dict()
 
 with open('TSE_sample_data.csv') as fhand:
+    
+    #Skip the first line of headers
     next(csv_f)
     
     for row in csv_f:
-        begin_date = int(row[13])
-        if begin_date < 1283731200:
-            if begin_date not in date2words:
-                date2words[begin_date] = row[16]
-
-    a_list = date2words.keys()
-    a_list.sort()
         
-for key in a_list:
-    print key, date2words[key]
+        #Note, all dates in TSE_sample_data.csv are represented as a unix timestamp.
+        #The 13th index of each row is the users start date.
+
+        start_date = int(row[13])
+        
+        #We can use a python comparison operator to identify users who started before 09-10-2010.
+        #The unix timestamp for 09-10-2010 is 1283731200.
+        
+        if start_date < 1283731200:                     #Check if user started before 09-10-2010.
+            date_and_word[start_date] = row[16]         #If so, create a new key-value pair for our dictionary.
+                                                        #Each key is a users start day. Each value is the corresponding word in the words column. 
+                                                    
+    a_list = date_and_word.keys()                       #Make a list of the keys, then sort it. 
+    a_list.sort()                                       
+        
+for key in a_list:                                      #Loop through the sorted list.
+    print key, date_and_word[key]                       #Print out key-value pairs in sorted order. 
