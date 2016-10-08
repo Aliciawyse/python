2e964d0db75f271e77d97edb30f45f35012b5e0c
Alicia <aliciawyse@gmail.com>
2016-10-08 23:35:58 +0000

    added comments

--- ch12/app_quiz.py
+++ ch12/app_quiz.py
@@ -1,4 +1,4 @@
-#Let python know we will be using the csv module in our program
+#Let python know we will use the csv module in our program.
 import csv
 
 #Create a dictionary to store key-value pairs. In this scenario, a key is a users start date 
@@ -11,27 +11,26 @@ with open('TSE_sample_data.csv') as my_csv_file:
     #Use the csv modules reader function to parse TSE_sample_data.csv and return a list of rows.
     csv_f = csv.reader(my_csv_file)
     
-    #Skip the first line of headers
+    #Disregard the first row because we are not interested in headers.
     next(csv_f)
     
     #For each row in TSE_sample_data.csv do the following steps 
     for row in csv_f:
     
-        #We know that all dates in TSE_sample_data.csv are represented as a unix timestamp and 
-        #the 13th index of each row is the users start date.
-    
+        #We know all dates in TSE_sample_data.csv are represented as a unix timestamp and 
+        #we know the 13th index of each row is the users start date and 
+        #the 16th index is the corresponding word from the words column. 
         start_date = int(row[13])
+        words_column_word = row[16]
         
-        #Use a python comparison operator to identify users who started before 09-10-2010.
-        #The unix timestamp for 09-10-2010 is 1283731200.
-        
+        #Use a python comparison operator to identify users who started before 09-10-2010. Its unix timestamp is 1283731200.
+
         #If the user started before 09-10-2010
         if start_date < 1283731200:           
             
             #Create a new key-value pair for our dictionary.
-            #Each key is a users start day. 
-            #Each value is the corresponding word in the 16th index of each row. 
-            date_and_word[start_date] = row[16]         
+            #A key is a users start day and a value is the corresponding word from the words column.
+            date_and_word[start_date] = words_column_word         
                                                         
     #Make a list of the keys then sort it.                                                     
     a_list = date_and_word.keys()                       
