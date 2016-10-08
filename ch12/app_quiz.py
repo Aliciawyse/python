6e104d01d5cddb2ae26d2ab67a2a0b6045a226db
Alicia <aliciawyse@gmail.com>
2016-10-08 22:36:12 +0000

    fixed mistakes

--- ch12/app_quiz.py
+++ ch12/app_quiz.py
@@ -1,28 +1,24 @@
 #Use pythons csv module to read and write data in csv format
 import csv
 
-#Use file handle to access data in TSE_sample_data.csv
-fhand = open('TSE_sample_data.csv')
-
-#Use the csv modules reader function to take each row of the TSE_sample_data.csv file 
-#and to make a list containing the values found in the all of the columns of that row. 
-csv_f = csv.reader(fhand)
 
 #Create a dictionary to store key-value pairs. In this scenario, a key is a users start date 
 #and a value is the corresponding word in the words column. 
 date_and_word = dict()
 
-
-with open('TSE_sample_data.csv') as fhand:
+with open('TSE_sample_data.csv', 'r') as my_csv_file:
+    
+    csv_f = csv.reader(my_csv_file)
     
     #Skip the first line of headers
     next(csv_f)
     
     for row in csv_f:
-        
+    
+
         #Note, all dates in TSE_sample_data.csv are represented as a unix timestamp.
         #The 13th index of each row is the users start date.
-
+    
         start_date = int(row[13])
         
         #We can use a python comparison operator to identify users who started before 09-10-2010.
