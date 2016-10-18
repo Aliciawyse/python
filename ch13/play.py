c51dd46d1e3b21f6f6a6ee27e2ec574197cc62d8
Alicia <aliciawyse@gmail.com>
2016-10-18 20:42:58 +0000

    adding practice on parsing data on the web using python

---
+++ ch13/play.py
@@ -0,0 +1,9 @@
+import urllib
+
+params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+
+f = urllib.urlopen("http://www.musi-cal.com/cgi-bin/query?%s" % params)
+
+print f
+print f.read()
+
